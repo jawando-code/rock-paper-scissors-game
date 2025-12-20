4957bb77c088b906176c2344aae713ab2537bd5f
jawando-code <peruvianswing@gmail.com>
2025-12-20 18:01:32 +0000

    Refactor alogorithims and add EventListeners
    Refactor alogorithim for GUI display due to errors.
    
    Add EventListeners to simultaneously trigger computer and human selection.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,32 +1,62 @@
+const createPara = document.createElement('p');
+const createPara2 = document.createElement('p')
+const computerContainer = document.getElementById('computer-container')
+
+function showComputerChoice(str) {
+    
+    createPara.textContent = str;
+    computerContainer.appendChild(createPara);
+        
+}
+
+const buttons = document.querySelectorAll('button');
+let humanChoice = '';
+buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+        humanChoice = `${button.value}`;
+      })
+
+
+button.addEventListener('click', () => {
+    createPara.textContent = `You select: ${humanChoice}`;
+    computerContainer.appendChild(createPara)
+
+})
+
+button.addEventListener('click', () => {
+    getComputerChoice();
+})
+
+button.addEventListener('click', () => {
+    createPara2.textContent = `Computer selects: ${computerChoice}`;
+    computerContainer.appendChild(createPara2)
+})
+
+button.addEventListener('click',() => {
+    playRound()
+})
+
 
 function getComputerChoice() {
   let computerResult = Math.floor(Math.random() * 3) +1
-
-if(computerResult === 1) {
-    return 'rock'
-} else if(computerResult === 2) {
-    return 'paper'
-} else if (computerResult === 3){
-    return 'scissors'
+    if(computerResult === 1) {
+        return 'rock'
+    } else if(computerResult === 2) {
+        return 'paper' 
+    } else if (computerResult === 3){
+        return 'rock'
+    }    
 }
-
-}
-
-// function getHumanChoice() {
-//     let userResult = prompt("Enter choice","",);
-//     return userResult;    
-// }
 
 
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
+
     const computerChoice = getComputerChoice().toLowerCase();
-    const humanChoice = ''
-    console.log(`Your choice: ${humanChoice}`);
-    console.log(`Computer's choice: ${computerChoice}`)
+
+function playRound() {
 
     if (computerChoice === humanChoice) {
         return console.log("It's a tie.")
@@ -37,7 +67,13 @@ function playRound() {
     ) {
         console.log(`SCORES: You: ${++humanScore}, Computer: ${computerScore}`)
         return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    } else {
+    } else if (
+        (computerChoice === 'rock' && humanChoice === 'scissors') ||
+        (computerChoice === 'scissors' && humanChoice === 'paper') ||
+        (computerChoice === 'paper' && humanChoice === 'rock')
+        )
+    
+{
        console.log(`SCORES: You: ${humanScore}, Computer: ${++computerScore}`)
         return console.log( `You lose! ${computerChoice} beats ${humanChoice}`)
         
@@ -63,15 +99,13 @@ playRound();
 
 // playGame()
 
-const buttons = document.querySelectorAll('button');
-// function getHumanChoice() {
-    let humanChoice = ''
-    buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        humanChoice = button.value;
-    })
+
+     
 })
 // }
+// para.textContent = `You selected ${buttons.value}`;
+//         document.container.appendChild(para);
+  
 
 
 
